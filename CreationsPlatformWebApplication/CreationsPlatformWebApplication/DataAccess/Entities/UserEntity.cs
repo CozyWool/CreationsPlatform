2d8223c20666e451e4b25924b240eea9a26d1469
CozyWool <vsergeev201530@gmail.com>
2024-08-19 15:14:15 +0500

    task-2 Добавил DbContext

---
+++ CreationsPlatformWebApplication/CreationsPlatformWebApplication/DataAccess/Entities/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace CreationsPlatformWebApplication.DataAccess.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public virtual ICollection<CommentEntity> Comments { get; set; } = new List<CommentEntity>();
+
+    public virtual ICollection<CreationEntity> Creations { get; set; } = new List<CreationEntity>();
+}
