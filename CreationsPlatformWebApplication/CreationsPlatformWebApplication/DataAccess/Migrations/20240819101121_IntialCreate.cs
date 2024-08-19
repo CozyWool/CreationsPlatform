882f55e4ac241466dc8834f6d5023305282ad1ef
CozyWool <vsergeev201530@gmail.com>
2024-08-19 15:14:15 +0500

    task-2 Добавил DbContext

---
+++ CreationsPlatformWebApplication/CreationsPlatformWebApplication/DataAccess/Migrations/20240819101121_IntialCreate.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CreationsPlatformWebApplication.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    login = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false),
+                    created_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Users_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Creations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    genre = table.Column<List<string>>(type: "character varying[]", nullable: false),
+                    publication_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    rating = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    rating_count = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    author_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Creations_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "author_id_fk",
+                        column: x => x.author_id,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    creation_id = table.Column<int>(type: "integer", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Comments_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "creation_id_fk",
+                        column: x => x.creation_id,
+                        principalTable: "Creations",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "user_id_fk",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_creation_id",
+                table: "Comments",
+                column: "creation_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_user_id",
+                table: "Comments",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Creations_author_id",
+                table: "Creations",
+                column: "author_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Creations");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
