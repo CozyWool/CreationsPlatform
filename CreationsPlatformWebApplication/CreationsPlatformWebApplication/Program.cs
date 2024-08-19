86a081dc1b13821f3249ccb30b11adabb0c37fb4
CozyWool <vsergeev201530@gmail.com>
2024-08-19 14:32:25 +0500

    Добавил проект-болванку

---
+++ CreationsPlatformWebApplication/CreationsPlatformWebApplication/Program.cs
@@ -0,0 +1,8 @@
+using CreationsPlatformWebApplication;
+
+CreateHostBuilder(args).Build().Run();
+return;
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
