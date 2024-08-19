2303be7fe8c2b86454e3417fbfabb70ffeb90b6e
CozyWool <vsergeev201530@gmail.com>
2024-08-19 18:18:53 +0500

    task-2 выполнен

---
+++ CreationsPlatformWebApplication/CreationsPlatformWebApplication/Models/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreationsPlatformWebApplication.Models;
+
+public class RegisterModel
+{
+    [Required(ErrorMessage = "Укажите Email")]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Укажите логин")]
+    [Display(Name = "Логин")]
+    public string Login { get; set; }
+
+    [Required(ErrorMessage = "Укажите пароль")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage = "Пароли не совпадают")]
+    public string ConfirmPassword { get; set; }
+}
--- CreationsPlatformWebApplication/CreationsPlatformWebApplication/Startup.cs
+++ CreationsPlatformWebApplication/CreationsPlatformWebApplication/Startup.cs
@@ -1,10 +1,32 @@
+using CreationsPlatformWebApplication.DataAccess.Contexts;
+using CreationsPlatformWebApplication.DataAccess.Repositories;
+using CreationsPlatformWebApplication.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 namespace CreationsPlatformWebApplication;
 
-public class Startup
+public class Startup(IConfiguration configuration)
 {
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddHttpContextAccessor();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserService, UserService>();
         
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.LoginPath = new PathString(CookieAuthenticationDefaults.LoginPath);
+                options.LogoutPath = new PathString(CookieAuthenticationDefaults.LogoutPath);
+                options.AccessDeniedPath = new PathString(CookieAuthenticationDefaults.AccessDeniedPath);
+                options.Cookie.HttpOnly = true;
+            });
+        services.AddMvc();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -15,12 +37,15 @@ public class Startup
         }
 
         app.UseStaticFiles();
-
         app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapGet("/", () => "Hello World!");
+            endpoints.MapControllerRoute(name: "default",
+                pattern: "{controller=CreationsPlatform}/{action=Index}/{id?}");
         });
     }
 }
